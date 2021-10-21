860303a5d0789e24006297963a758fb00cd3b05b
Quinck Mac Mini <quinckmacmini@Mini-di-Quinck.home>
2021-10-21 17:29:00 +0200

    NFTcard element completition

--- src/components/NFTcard/NFTcard.js
+++ src/components/NFTcard/NFTcard.js
@@ -1,4 +1,12 @@
-import ' ./NFTcard.css';
+import './NFTcard.css';
+import saveIcon from '../../assets/save-icon.png';
+import heartIcon from '../../assets/heart-icon.png';
+import galleryIcon from '../../assets/gallery-icon.png';
+import ethereumIcon from '../../assets/ethereum-logo.png';
+import volumeIcon from '../../assets/volume-icon.png';
+import threePointsIcon from '../../assets/three-points-icon.png';
+import user1ProfileImage from '../../assets/user-profile-image.png';
+import crownIcon from '../../assets/crown-icon.png';
 
 function NFTcard(){
 
@@ -8,39 +16,87 @@ function NFTcard(){
 
             <div className="top-card">
 
-                <img src="card-three-points" className="left-card-icon"/>
+                <img src={threePointsIcon} className="left-card-icon"/>
 
-                <h3 className="card-hearts-number">492</h3>
+                <div className="right-card-icon-set">
 
-                <img src="card-hearts-icon" className="right-card-icon"/>
+                    <h3 className="card-hearts-number">492</h3>
 
-                <img src="card-save-icon" className="right-card-icon"/>
+                    <img src={heartIcon} className="right-card-icon-heart" />
+
+                    <img src={saveIcon} className="right-card-icon-save"/>
+
+                </div>
 
             </div>
 
             <div className="card-image-container">
 
-                <img src="card-image-1" className="card-image"/>
+                <img scr={galleryIcon} className="gallery-card-icon"/>
 
-                <img src="card-sound-try-icon" className="volume-card-icon"/>
+                <img src={volumeIcon}className="volume-card-icon"/>
 
             </div>
 
-            <div className="bottom-card"> 
+            <div className="bottom-card">                                                             
 
-            <span className="username-artist"></span>
+                <div className="left-bottom-card-side">
 
-            <span className="title-NFT"></span>
-            
-            <span className="text-about-sells"></span>
-            
-            <span className="text-about-sells"></span>
+                    <div className="completed-artist-info-container">                                            
 
-            <img src="etherium-icon" className="etherium-icon"/>
-            
-            <span className="text-about-sells"></span>
-            
-            <img src="etherium-icon" className="etherium-icon"/>
+                        <img src={user1ProfileImage} className="artist-profile-image"/>
+
+                        <div className="artist-info-plus-NFTtitle-container" >                                   
+
+                            <div className="artist-info-container">     
+
+                                <span className="username-artist">username_artista</span>
+
+                                <img src={crownIcon} className="crown-icon"/>
+
+                            </div>
+
+                            <span className="title-NFT">Titolo NFT</span>
+
+                        </div>
+
+                    </div>
+                        
+                    <span className="leftside-text-about-sells" className="container-without-underline-link">venduto da <a href="link-artista" className="special-link">username</a></span>
+
+                </div>
+                
+                <div className="right-bottom-card-side">
+
+                    <div className="completed-sells-container">
+
+                        <span className="rightside-text-about-sells">Price</span>
+
+                        <div className= "sells-info-container">
+
+                            <img src={ethereumIcon } className="etherium-icon"/>
+
+                            <span className="etherium-number">1,4553</span>
+
+                        </div>
+
+                    </div>
+
+                    <div className="completed-sells-container">
+
+                        <span className="rightside-text-about-sells">Ultima offerta</span>
+
+                        <div className= "sells-info-container">
+                        
+                            <img src={ethereumIcon } className="etherium-icon"/>
+
+                            <span className="etherium-number">1,4553</span>
+
+                        </div>
+
+                    </div>
+
+                </div>
 
             </div>
 
@@ -48,3 +104,4 @@ function NFTcard(){
 
     );
 }
+export default NFTcard;
